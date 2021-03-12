d93108ace2b252663314f81a0d653f1c66e6ccac
Dave Nunez <noonz2@icloud.com>
2021-03-12 16:46:23 -0500

    init commit

---
+++ app/src/main/java/com/example/case_study_global_news/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.case_study_global_news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
