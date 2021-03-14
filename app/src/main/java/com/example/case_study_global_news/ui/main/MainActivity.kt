38865343a4ac1b2a5bf4182eb34ee936e5b78197
ZCai <zc128@users.noreply.github.com>
2021-03-13 22:08:59 -0500

    set up list adapter and basic layout

--- app/src/main/java/com/example/case_study_global_news/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/case_study_global_news/ui/main/MainActivity.kt
@@ -2,13 +2,22 @@ package com.example.case_study_global_news.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.case_study_global_news.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private val data = IntArray(100_000) { it }.toList().map { "Item $it" }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val newsRecyclerView:RecyclerView = findViewById(R.id.news_recycler_view)
+        newsRecyclerView.layoutManager = LinearLayoutManager(this)
 
+        val adapter = NewsCardAdapter()
+        adapter.data = data
+        newsRecyclerView.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/example/case_study_global_news/ui/main/NewsCardAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.case_study_global_news.ui.main
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.case_study_global_news.R
+import kotlin.random.Random
+import kotlin.random.Random.Default.nextInt
+
+class NewsCardAdapter:RecyclerView.Adapter<NewsViewHolder>() {
+    var data: List<String> = emptyList()
+    override fun onViewRecycled(holder: NewsViewHolder) {
+        super.onViewRecycled(holder)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        return NewsViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        holder.bind(data[position], position)
+    }
+
+    override fun getItemCount(): Int = data.size
+}
+class NewsViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    companion object {
+        fun from(parent: ViewGroup): NewsViewHolder {
+            val itemView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.news_card_item, parent, false)
+            val titleView: TextView = itemView.findViewById(R.id.news_card_title)
+            titleView.text = Random.nextInt(1000).toString()
+            return NewsViewHolder(itemView)
+        }
+    }
+
+    private val textView: TextView = itemView.findViewById(R.id.news_card_date)
+
+    fun bind(value: String, positon: Int) {
+        textView.text = positon.toString()
+    }
+}
